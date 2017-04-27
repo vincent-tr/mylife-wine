48076ee484eaac11326737de1a334e7c839d2635
Vincent Trumpff <vincent@stoic.com>
2017-04-27 17:20:17 +0200

    style

--- public/app/components/reference/type-list.js
+++ public/app/components/reference/type-list.js
@@ -8,34 +8,10 @@ import icons from '../icons';
 const TypeList = ({ value, onChange }) => (
   <base.SelectableList selectedNode={{value}}
             selectedValueChanged={(obj) => onChange(obj.value)}>
-    <base.SelectableListItem value={{ value: 'capacities'}}
-      leftIcon={
-        <base.TooltipContainer tooltip="Capacites de bouteilles">
-          <icons.tabs.Management />
-        </base.TooltipContainer>
-      }
-      primaryText="Capacites de bouteilles"/>
-    <base.SelectableListItem value={{ value: 'dishes'}}
-      leftIcon={
-        <base.TooltipContainer tooltip="Plats">
-          <icons.tabs.Management />
-        </base.TooltipContainer>
-      }
-      primaryText="Plats"/>
-    <base.SelectableListItem value={{ value: 'regions'}}
-      leftIcon={
-        <base.TooltipContainer tooltip="Regions">
-          <icons.tabs.Management />
-        </base.TooltipContainer>
-      }
-      primaryText="Regions"/>
-    <base.SelectableListItem value={{ value: 'types'}}
-      leftIcon={
-        <base.TooltipContainer tooltip="Types">
-          <icons.tabs.Management />
-        </base.TooltipContainer>
-      }
-      primaryText="Types"/>
+    <base.SelectableListItem value={{ value: 'capacities'}} primaryText="Capacites de bouteilles"/>
+    <base.SelectableListItem value={{ value: 'dishes'}}     primaryText="Plats"/>
+    <base.SelectableListItem value={{ value: 'regions'}}    primaryText="Regions"/>
+    <base.SelectableListItem value={{ value: 'types'}}      primaryText="Types"/>
   </base.SelectableList>
 );
 
