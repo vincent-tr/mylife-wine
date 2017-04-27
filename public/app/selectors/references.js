1e03cd03193d4a6304861b1721642d32ad1844e9
Vincent Trumpff <vincent@stoic.com>
2017-04-27 11:33:41 +0200

    sort references

--- public/app/selectors/references.js
+++ public/app/selectors/references.js
@@ -1,6 +1,8 @@
 'use strict';
 
-export const getCapacities = (state) => state.capacities.toArray();
-export const getDishes     = (state) => state.dishes.toArray();
-export const getRegions    = (state) => state.regions.toArray();
-export const getTypes      = (state) => state.types.toArray();
+export const getCapacities = (state) => state.capacities.valueSeq().sortBy(item => item.value).toArray();
+export const getDishes     = (state) => state.dishes.valueSeq().sortBy(item => item.name).toArray();
+export const getRegions    = (state) => state.regions.valueSeq().sortBy(item => item.name).toArray();
+export const getTypes      = (state) => state.types.valueSeq().sortBy(item => item.name).toArray();
+
+
